155bc162cd178aab4089c0284b148533472154a4
adechastellier <90505102+adechastellier@users.noreply.github.com>
2021-09-26 17:55:25 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,67 @@
 input.onButtonPressed(Button.A, function () {
     Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, 30)
 })
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "monte") {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (receivedString == "descend") {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+    } else if (receivedString == "eloigne") {
+        basic.showLeds(`
+            # # # . .
+            # # # # .
+            # # # # #
+            # # # # .
+            # # # . .
+            `)
+    } else if (receivedString == "rapproche") {
+        basic.showLeds(`
+            . . # # #
+            . # # # #
+            # # # # #
+            . # # # #
+            . . # # #
+            `)
+    } else if (receivedString == "gauche") {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+    } else if (receivedString == "droite") {
+        basic.showLeds(`
+            . . . . .
+            . # # . .
+            # # # # #
+            . # # . .
+            . . . . .
+            `)
+    } else if (receivedString == "stop") {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # # # # #
+            . . # . .
+            . . # . .
+            `)
+    }
+})
 input.onButtonPressed(Button.B, function () {
     Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Reverse, 30)
 })
+radio.setGroup(1)
 Kitronik_Move_Motor.stop()
